518918b11e50eebeb78a666e2207968e9c2181f4
purapetino <115918379+purapetino@users.noreply.github.com>
2022-12-09 23:52:40 +0900

    fix: configチェックを修正

--- src/utils/ConfigUtils.ts
+++ src/utils/ConfigUtils.ts
@@ -58,27 +58,6 @@ export class ConfigUtils {
     if(config.settings.check_status.enable == false){
       Logger.Debug("Skip: Check API URL");
       Logger.SystemWarn("現在ステータスチェックが無効化されています。本番環境での使用はおすすめしません");
-    } else {
-      if(!config.settings.check_status.bot_status_URL.startsWith("http")){
-        Logger.SystemError("BotのステータスURLが正しくありません。URLの値はhttpもしくはhttpsから始まる必要があります。");
-        process.exit(1);
-      }
-
-      if(!config.settings.check_status.bot_status_URL.endsWith("/api/v1/status")){
-        Logger.SystemError("BotのステータスURLが正しくありません。URLの値にはAPIのエンドポイントを含む必要があります。本番環境では正しく動作しない恐れがあります");
-      } 
-
-      if(!config.settings.check_status.frontend_status_URL.startsWith("http")){
-        Logger.SystemError("webサーバーのステータスURLが正しくありません。URLの値はhttpもしくはhttpsから始まる必要があります。");
-        process.exit(1);
-      }
-
-      if(!config.settings.check_status.frontend_status_URL.endsWith("/api/v1/status")){
-        Logger.SystemError("WebサーバーのステータスURLが正しくありません。URLの値にはAPIのエンドポイントを含む必要があります。本番環境では正しく動作しない恐れがあります");
-      }
-
-      Logger.Debug("Passing: Status URL");
-
       if(config.settings.check_status.disabled_status == "up" || config.settings.check_status.disabled_status == "warn" || config.settings.check_status.disabled_status == "down"){
         Logger.Debug("Passing: Status (config:15)");
       } else {
