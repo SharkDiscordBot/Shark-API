a58810ee8eab76a2c9ea19cbb65502d512170783
purapetino <115918379+purapetino@users.noreply.github.com>
2023-01-05 14:30:50 +0900

    feat: ライセンスチェックをプルリク時に行うように

---
+++ check_license.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DISALLOW_LICENSES="GPL;GPL-2.0;GPL-3.0;LGPL;LGPL-3.0;AGPL;NGPL;"
+
+result=0
+
+npx license-checker --production --direct --json --failOn "$DISALLOW_LICENSES" >/dev/null || result=$?
+
+if [[ ! "$result" = "0" ]]; then
+  echo "エラー: 許容しないライセンスが含まれています"
+  exit 1
+fi
+
+exit 0
